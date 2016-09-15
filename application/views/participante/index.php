d7c845c890576d5827c66a62bdc4c6992484f504
José Guilherme <Jos Guilherme@DESKTOP-Q9R0D3V>
2016-09-15 18:43:44 -0300

    Continuação do Layout da Página de Início do Participante!

--- application/views/participante/index.php
+++ application/views/participante/index.php
@@ -1,5 +1,70 @@
 <h2><span class="glyphicon glyphicon-home"></span><b> Início</b></h2>
 <hr>
 <br>
-<div class="postit"><i class="pin"></i>Este é um teste!</div>
+<h4><span class="glyphicon glyphicon-calendar"></span><b> Datas Importantes</b></h4><br>
+<div class="row">
+<div class="col-md-3 col-sm-4 col-xs-6">
+<div class="postit"><i class="pin"></i>
+    <div class="postit-conteudo">
+        <div class="postit-data">
+            28
+        </div>
+        <div class="postit-mes-ano">
+            Fev 2016
+        </div>
+        <div class="postit-detalhes">
+            Entrega final do Trabalho.
+        </div>
+    </div>
+</div>
+</div>
+<div class="col-md-3 col-sm-4 col-xs-6">
+<div class="postit"><i class="pin"></i>
+    <div class="postit-conteudo">
+        <div class="postit-data">
+            28
+        </div>
+        <div class="postit-mes-ano">
+            Fev 2016
+        </div>
+        <div class="postit-detalhes">
+            Entrega final do Trabalho.
+        </div>
+    </div>
+</div>
+</div>
+<div class="col-md-3 col-sm-4 col-xs-6">
+<div class="postit"><i class="pin"></i>
+    <div class="postit-conteudo">
+        <div class="postit-data">
+            28
+        </div>
+        <div class="postit-mes-ano">
+            Fev 2016
+        </div>
+        <div class="postit-detalhes">
+            Entrega final do Trabalho.
+        </div>
+    </div>
+</div>
+</div>
+<div class="col-md-3 col-sm-4 col-xs-6">
+<div class="postit"><i class="pin"></i>
+    <div class="postit-conteudo">
+        <div class="postit-data">
+            28
+        </div>
+        <div class="postit-mes-ano">
+            Fev 2016
+        </div>
+        <div class="postit-detalhes">
+            Entrega final do Trabalho.
+        </div>
+    </div>
+</div>
+</div>
+</div>
+<br><br>
+<h4><span class="glyphicon glyphicon-alert"></span><b> Notificações</b></h4><br>
+
 
