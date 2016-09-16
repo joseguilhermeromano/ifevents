acb2ed967722835ebf5089cd3a845c4cd98850b5
José Guilherme <Jos Guilherme@DESKTOP-Q9R0D3V>
2016-09-16 18:46:41 -0300

    Continuação do layout do participante! Montagem das Páginas Inicio, Meus Artigos e Contato.

--- application/views/participante/meusartigos.php
+++ application/views/participante/meusartigos.php
@@ -1,4 +1,66 @@
-<h2><span class="glyphicon glyphicon-list"></span><b> Meus Artigos</b></h2>
+<h2><span class="glyphicon glyphicon-list"></span><b> Minhas Submissões</b></h2>
 <hr>
 <br>
-
+<div class="row">
+<div class="col-sm-5">
+    
+    <div class="input-group h2">
+        <input name="codigo" class="form-control" id="search" type="text" placeholder="Buscar por Título">
+        <span class="input-group-btn">
+            <button class="btn btn-success" type="submit">Buscar</button>
+        </span>
+    </div>
+</div>
+<div class="col-sm-7">
+    <a href="#" class="btn btn-success pull-right h2"><span class="glyphicon glyphicon-plus"></span> Nova Submissão</a>
+</div>
+</div>
+<br><br>
+<div class="table-responsive"><!-- TABELA-->
+    <table class="table ls-table" id="tabela1">
+        <thead>
+            <tr>
+                    <th class="txt-center"><input type="checkbox"></th>
+                    <th>Título</th>
+                    <th>Campanha</th>
+                    <th>Status</th>
+                    <th>Data de envio</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="primeira-cor">
+                    <td class="txt-center"><input type="checkbox"></td>
+                    <td><a href="" title="">Mensagem Teste Locastyle</a></td>
+                    <td class="">Campanha</td>
+                    <td>Enviada</td>
+                    <td class="">21/09/2013 as 20:00 PM</td>
+            </tr>
+            <tr  class="segunda-cor">
+                    <td class="txt-center"><input type="checkbox"></td>
+                    <td><a href="" title="">Mensagem Teste Locastyle</a></td>
+                    <td class="">Campanha</td>
+                    <td>Enviada</td>
+                    <td class="">21/09/2013 as 20:00 PM</td>
+            </tr>
+        </tbody>
+    </table>
+</div><!-- /TABELA-->
+<nav><!-- Paginação -->
+    <ul class="pagination">
+      <li>
+        <a href="#" aria-label="Previous">
+          <span aria-hidden="true">&laquo;</span>
+        </a>
+      </li>
+      <li><a href="#">1</a></li>
+      <li><a href="#">2</a></li>
+      <li><a href="#">3</a></li>
+      <li><a href="#">4</a></li>
+      <li><a href="#">5</a></li>
+      <li>
+        <a href="#" aria-label="Next">
+          <span aria-hidden="true">&raquo;</span>
+        </a>
+      </li>
+    </ul>
+</nav><!-- /Paginação -->
