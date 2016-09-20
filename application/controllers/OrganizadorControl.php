123a8cea846931bd4214c8a6f79965ce18f7e2fa
José Guilherme <Jos Guilherme@DESKTOP-Q9R0D3V>
2016-09-20 18:51:50 -0300

    Hoje precisei estudar melhor a estrutura dos sistemas similares e editei algumas coisas no controlador do organizador, menu e suas páginas!

--- application/controllers/OrganizadorControl.php
+++ application/controllers/OrganizadorControl.php
@@ -17,5 +17,46 @@
                 $this->load->view("common/footer_interno");
             }
             
+            public function avaliadores(){
+                $this->load->view("common/header_interno");
+                $this->load->view("organizador/avaliadores");
+                $this->load->view("common/footer_interno");
+            }
+            
+            public function conferencias(){
+                $this->load->view("common/header_interno");
+                $this->load->view("organizador/conferencias");
+                $this->load->view("common/footer_interno");
+            }
+            
+            public function contatos(){
+                $this->load->view("common/header_interno");
+                $this->load->view("organizador/contatos");
+                $this->load->view("common/footer_interno");
+            }
+            
+            public function meuperfil(){
+                $this->load->view("common/header_interno");
+                $this->load->view("organizador/meuperfil");
+                $this->load->view("common/footer_interno");
+            }
+            
+            public function novaconferencia(){
+                $this->load->view("common/header_interno");
+                $this->load->view("organizador/novaconferencia");
+                $this->load->view("common/footer_interno");
+            }
+            
+            public function participantes(){
+                $this->load->view("common/header_interno");
+                $this->load->view("organizador/participantes");
+                $this->load->view("common/footer_interno");
+            }
+            
+            public function respondercontato(){
+                $this->load->view("common/header_interno");
+                $this->load->view("organizador/respondercontato");
+                $this->load->view("common/footer_interno");    
+            }
     }
 
--- application/views/organizador/meuperfil.php
+++ application/views/organizador/meuperfil.php
@@ -1,8 +1,121 @@
-<?php
+<h2><span class="glyphicon glyphicon-user"></span><b> Meu Perfil</b><a class="btn btn-default" style="float:right">Alterar Senha</a></h2>
+<hr>
+<br>
+<div class="row">
+    <div class="col-md-12">
+        <div class="form-group">
+        <b><?php echo form_label( 'Nome Completo', 'subm_titulo' ); ?></b>
+        <?php $data = array( 'name' => 'subm_titulo', 'placeholder' => "Nome Completo", 'class' => 'form-control estilo-input' );
+               echo form_input($data);?>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-4">
+        <div class="form-group">
+        <b><?php echo form_label( 'RG', 'subm_orientador' ); ?></b><br>
+        <?php   $data = array( 'name' => 'subm_orientador', 'placeholder' => 'RG', 'class' => 'form-control estilo-input' );
+                    echo form_input( $data );  ?>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="form-group">
+        <b><?php echo form_label( 'CPF', 'subm_instituicao' ); ?></b>
+        <?php $data = array( 'name' => 'subm_instituicao', 'placeholder' => 'CPF','class' => 'form-control estilo-input' );
+              echo form_input( $data );?>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="form-group">
+        <b><?php echo form_label( 'Instituição/Empresa', 'subm_orientador' ); ?></b><br>
+        <?php   $data = array( 'name' => 'subm_orientador', 'placeholder' => 'Instituição/Empresa', 'class' => 'form-control estilo-input' );
+                    echo form_input( $data );  ?>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-4">
+        <div class="form-group">
+        <b><?php echo form_label( 'E-mail', 'subm_instituicao' ); ?></b>
+        <?php $data = array( 'name' => 'subm_instituicao', 'placeholder' => 'E-mail','class' => 'form-control estilo-input' );
+              echo form_input( $data );?>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="form-group">
+        <b><?php echo form_label( 'Telefone', 'subm_instituicao' ); ?></b>
+        <?php $data = array( 'name' => 'subm_instituicao', 'placeholder' => 'Telefone','class' => 'form-control estilo-input' );
+              echo form_input( $data );?>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="form-group">
+        <b><?php echo form_label( 'Celular', 'subm_instituicao' ); ?></b>
+        <?php $data = array( 'name' => 'subm_instituicao', 'placeholder' => 'Celular','class' => 'form-control estilo-input' );
+              echo form_input( $data );?>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+        <div class="form-group">
+        <b><?php echo form_label( 'Endereço', 'subm_titulo' ); ?></b>
+        <?php $data = array( 'name' => 'subm_titulo', 'placeholder' => "Endereço", 'class' => 'form-control estilo-input' );
+               echo form_input($data);?>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-4">
+        <div class="form-group">
+        <b><?php echo form_label( 'Número', 'subm_titulo' ); ?></b>
+        <?php $data = array( 'name' => 'subm_titulo', 'placeholder' => "Número", 'class' => 'form-control estilo-input' );
+               echo form_input($data);?>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="form-group">
+        <b><?php echo form_label( 'Bairro', 'subm_titulo' ); ?></b>
+        <?php $data = array( 'name' => 'subm_titulo', 'placeholder' => "Bairro", 'class' => 'form-control estilo-input' );
+               echo form_input($data);?>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="form-group">
+        <b><?php echo form_label( 'Complemento', 'subm_titulo' ); ?></b>
+        <?php $data = array( 'name' => 'subm_titulo', 'placeholder' => "Complemento", 'class' => 'form-control estilo-input' );
+               echo form_input($data);?>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-4">
+        <div class="form-group">
+        <b><?php echo form_label( 'CEP', 'subm_titulo' ); ?></b>
+        <?php $data = array( 'name' => 'subm_titulo', 'placeholder' => "CEP", 'class' => 'form-control estilo-input' );
+               echo form_input($data);?>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="form-group">
+        <b><?php echo form_label( 'Cidade', 'subm_titulo' ); ?></b>
+        <?php $data = array( 'name' => 'subm_titulo', 'placeholder' => "Cidade", 'class' => 'form-control estilo-input' );
+               echo form_input($data);?>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="form-group">
+        <b><?php echo form_label( 'Estado', 'subm_titulo' ); ?></b>
+        <?php $data = array( 'name' => 'subm_titulo', 'placeholder' => "Estado", 'class' => 'form-control estilo-input' );
+               echo form_input($data);?>
+        </div>
+    </div>
+</div>
+<?php echo '<br><center>'.form_submit("btn_atualizar", "Atualizar",array('class' => 'btn btn-success button'))."</center>";
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+      echo form_fieldset_close();
+
+      echo form_close();
+?>
+
 
