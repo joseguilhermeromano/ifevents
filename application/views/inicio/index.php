db267fba45606d54a68bdb64a32580a9e41732a3
José Guilherme <Jos Guilherme@DESKTOP-Q9R0D3V>
2017-02-22 11:35:13 -0300

    algumas correções no layout externo!

--- application/views/inicio/index.php
+++ application/views/inicio/index.php
@@ -1,15 +1,7 @@
-<!-- Header -->
-<!-- <header class="w3-display-container w3-content w3-wide" style="max-width:1500px;" id="home">
-  <img class="w3-image" src="https://w3schools.com/w3images/architect.jpg" alt="Architecture" width="1500" height="800">
-  <div class="w3-display-middle w3-margin-top w3-center">
-    <h1 class="w3-xxlarge w3-text-white"><span class="w3-padding w3-black w3-opacity-min"><b>BR</b></span> <span class="w3-hide-small w3-text-light-grey">Architects</span></h1>
-  </div>
-</header> -->
-
 <div id="inicio">
   <div class="inicio-header">
     <div class="banner">
-      <img class="img-responsive banner-image" src="<?php echo base_url('assets/area-externa/img/banner-index.jpg'); ?>" width="100%" height="600px">
+      <img class="img-responsive banner-image" src="<?php echo base_url("assets/area-externa/img/banner-index.jpg"); ?>" width="100%">
     </div>
     <div class="display-middle margin-top centralizar">
       <h4>EVENTOS</h4>
